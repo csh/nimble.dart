18d127ae7104d19fd3fd4fefcc8ece8f2dfa0caf
Connor Spencer Harries <connorsharries96@gmail.com>
2018-04-10 16:18:38 +0100

    Build using Travis
    Refactor our lonesome test file now that I've actually read the package documentation.

--- test/test_load.dart
+++ test/test_load.dart
@@ -1,20 +1,19 @@
-import 'dart:async';
 import 'dart:io';
 
 import 'package:nimble/loader.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('PluginManager.load() correctly isolates and loads a plugin', () async {
+  test('PluginManager.load() correctly isolates and loads a plugin', () {
     var directory = new Directory("test-plugin");
     var manager = new PluginManager();
-    var plugin = await manager.load(directory);
-    var completer = new Completer<String>();
-    plugin.listen("test", (channel, data) {
-      completer.complete(data["message"]);
-    });
-    expect(completer.future, completion(equals("Hello World")));
-    plugin.send("nimble:control", {"command": "terminate"});
+    manager.load(directory).then(expectAsync1((Plugin plugin) {
+      expect(plugin.name, equals("test"));
+      plugin.listen("test", expectAsync2((_, data) {
+        expect(data["message"], equals("Hello World"));
+        plugin.send("nimble:control", {"command": "terminate"});
+      }));
+    }));
   });
 }
  
